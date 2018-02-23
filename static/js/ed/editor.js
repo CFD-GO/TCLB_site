353e91e790fdf3fed6c142a4b03eb96619a982c0
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2018-02-23 13:27:05 +0100

    nice login loading

--- static/js/ed/editor.js
+++ static/js/ed/editor.js
@@ -1,11 +1,29 @@
 
+var tclb_sec_cookie = true;
+var tclb_im_sec = false;;
+
+$(function() {
+  if (location.hostname == "localhost") {
+    tclb_sec_cookie = false;
+    tclb_im_sec = true;
+  } else {
+    if (location.protocol == "https:") {
+      tclb_im_sec = true;
+    }
+  }
+})
+
+function tclb_go_sec() {
+  if (location.protocol !== "https:") location.protocol = "https:";
+}
+
 function tclb_editor_login () {
       var authenticator = new netlify.default ({site_id: "tclb.io"});
       authenticator.authenticate({provider:"github", scope: "user"}, function(err, data) {
         if (err) {
           return alert("Error Authenticating with GitHub: " + err);
         }
-        Cookies.set('gh_token',data.token, { secure: true });
+        Cookies.set('gh_token',data.token, { secure: tclb_sec_cookie });
         tclb_editor_check_login();
       });
 }
@@ -25,7 +43,6 @@ function tclb_editor_save () {
 }
 
 $(function() {
-  $("#nav-login a").attr("href", "javascript:tclb_editor_login();");
   $("ul.navbar-nav").append(
     $("<li>", { class: "nav-item dropdown", id: "nav-logout" }).append(
       $("<a>", { class: "nav-link dropdown-toggle", id:"navbarDropdownMenuLink", 'data-toggle':"dropdown" })
@@ -47,39 +64,49 @@ $(function() {
   );
 })
 
+
 function tclb_editor_disp_prof(profile) {
           $("#nav-profile-avatar").attr("src",profile.avatar_url);
           $("#nav-logout").show();
 }
 
 function tclb_editor_check_login() {
-  var gh_token = Cookies.get('gh_token');
-  if (gh_token) {
-    console.log(gh_token);
-    $("#nav-login").hide();
-    profile =  Cookies.getJSON('gh_profile');
-    if (profile) {
-      tclb_editor_disp_prof(profile);
+  if (tclb_im_sec) {
+    var gh_token = Cookies.get('gh_token');
+    if (gh_token) {
+      console.log(gh_token);
+      $("#nav-login").hide();
+      profile =  Cookies.getJSON('gh_profile');
+      if (profile) {
+        tclb_editor_disp_prof(profile);
+      } else {
+        var gh = new GitHub({
+          token: gh_token
+        });
+        var me = gh.getUser();
+        console.log(me);
+        me.getProfile(function(err, profile) {
+          if (! err) {
+            console.log(profile);
+            tclb_editor_disp_prof(profile);
+            Cookies.set('gh_profile',profile, { secure: tclb_sec_cookie });
+          } else {
+            console.log(err);
+          }
+        });
+      }
     } else {
-      var gh = new GitHub({
-        token: gh_token
-      });
-      var me = gh.getUser();
-      console.log(me);
-      me.getProfile(function(err, profile) {
-        if (! err) {
-          console.log(profile);
-          tclb_editor_disp_prof(profile);
-          Cookies.set('gh_profile',profile, { secure: true });
-        } else {
-          console.log(err);
-        }
-      });
+      $("#nav-login a").attr("href", "javascript:tclb_editor_login();");
+      $("#nav-login a i").attr("class", "now-ui-icons users_circle-08");
+      $("#nav-login").show();
+      $("#nav-logout").hide();
     }
   } else {
-    $("#nav-login").show();
-    $("#nav-logout").hide();
-  }
+      $("#nav-login a").attr("href", "javascript:go_sec();");
+//      $("#nav-login a i").attr("class", "now-ui-icons users_circle-08");
+      $("#nav-login").show();
+      $("#nav-logout").hide();
+  }    
 }
 
 $(function() { tclb_editor_check_login(); })
